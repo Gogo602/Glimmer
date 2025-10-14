aa9335b65a89ca8315ea91ba90049225fcb2fc49
Gogo602 <gogomarosi61@gmail.com>
2025-10-14 18:40:11 +0300

    how it works session

--- components/home/HowItWorks.jsx
+++ components/home/HowItWorks.jsx
@@ -19,12 +19,12 @@ export default function HowItWorks() {
                 </p>
                   
                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-7 gap-5 md:px-10">
-                    {whyUs.map((why) => (
-                        <div key={why.id} className=" p-5 shadow-2xl border-t-4 border-[#270A63E5] relative">
+                    {howItWorks.map((how) => (
+                        <div key={how.id} className=" p-5 shadow-2xl border-t-4 border-[#270A63E5] relative">
                             <FaPlus className="absolute h-6 w-6 -top-3.5 -left-3.5 text-yellow-600 md:hidden" />
-                            <p className="flex items-center justify-center">{why.icon}</p>
-                            <p className="font-bold py-5 text-lg text-center">{why.title}</p>
-                            <p className="text-center">{why.why}</p>
+                            <p className="flex items-center justify-center">{how.icon}</p>
+                            <p className="font-bold py-5 text-lg text-center">{how.title}</p>
+                            <p className="text-center">{how.how}</p>
                         </div> 
                     ))}
                </div>
@@ -41,23 +41,23 @@ export default function HowItWorks() {
   )
 }
 
-const whyUs = [
+const howItWorks = [
     {
         id: 1,
         icon: <FaUserEdit size={45} className="border border-[#270A63E5] bg-[#270A63E5]/20 rounded-full p-2"/>,
         title: "Step 1: Fill in your details",
-        why: "Provide your professional information, career goals and style preferences"
+        how: "Provide your professional information, career goals and style preferences"
     },
     {
         id: 2,
         icon: <IoSparklesSharp size={45} className="border border-[#270A63E5] bg-[#270A63E5]/20 rounded-full p-2"/>,
         title: "Step 2: Let AI design your banner & bio",
-        why: "Our AI generates a uniques banner and a compeling bio taloired to you"
+        how: "Our AI generates a uniques banner and a compeling bio taloired to you"
     },
     {
         id: 3,
         icon: <FaCircleArrowDown size={45} className="border border-[#270A63E5] bg-[#270A63E5]/20 rounded-full p-2"/>,
         title: "Step 3: Preview, download, and publish",
-        why: "Review your new assets, download them and ipdate your profiles instantly"
+        how: "Review your new assets, download them and ipdate your profiles instantly"
     }
 ]
